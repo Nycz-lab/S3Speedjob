8bada94947a6a42795b2b4a812b574d6dd430cfc
Nick <63111944+Nycz-lab@users.noreply.github.com>
2021-11-28 17:29:36 +0100

    frontend lsd update

--- speedjob/speedjob/settings.py
+++ speedjob/speedjob/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['s3jobs.ddns.net', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['s3jobs.ddns.net', '127.0.0.1', 'localhost', '192.168.2.114']
 
 
 # Application definition
