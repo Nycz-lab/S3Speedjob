f644eaa1633782c61b9b03160abe77aa554715d8
Nycz-lab <Skybreak00@protonmail.com>
2021-12-14 16:27:49 +0100

    Revert "Update settings.py"
    This reverts commit 3946d5ce9d3ceae5129bafcf1ef0efbded093ed4.

--- speedjob/settings.py
+++ speedjob/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 
 from decouple import config
-import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,9 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
