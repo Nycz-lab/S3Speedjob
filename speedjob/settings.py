7ed30ebfa7a0a20e290f0eb0c7c4bba802f39a68
Nycz-lab <Skybreak00@protonmail.com>
2021-12-14 16:52:06 +0100

    Update settings.py

--- speedjob/settings.py
+++ speedjob/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DJANGO-DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = ['s3speedjob.herokuapp.com']
 
