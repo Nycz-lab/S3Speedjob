12e755433e504bab760c24f340ad99b26555c66f
Nycz-lab <Skybreak00@protonmail.com>
2021-12-14 14:47:10 +0100

    Revert "prep for heroku"
    This reverts commit 480bd0b91059080bd12adee717bea71a94fa1d06.

--- speedjob/speedjob/settings.py
+++ speedjob/speedjob/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['s3jobs.ddns.net', '127.0.0.1', 'localhost', 's3speedjob.herokuapp.com']
+ALLOWED_HOSTS = ['s3jobs.ddns.net', '127.0.0.1', 'localhost', '192.168.2.114']
 
 
 # Application definition
@@ -51,8 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #added following
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'speedjob.urls'
